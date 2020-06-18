914de8913f82c71ea865b1cd2256d051d154f1bb
Thomas Roehr <thomas.roehr@dfki.de>
2020-06-18 16:00:06 +0200

    Adapt for templ-buildconf

--- init.rb
+++ init.rb
@@ -1,29 +1,37 @@
-# Write in this file customization code that will get executed before 
-# autoproj is loaded.
-
-# Set the path to 'make'
-# Autobuild.commands['make'] = '/path/to/ccmake'
-
-# Set the parallel build level (defaults to the number of CPUs)
-# Autobuild.parallel_build_level = 10
-
-# Uncomment to initialize the environment variables to default values. This is
-# useful to ensure that the build is completely self-contained, but leads to
-# miss external dependencies installed in non-standard locations.
-#
-# set_initial_env
-#
-# Additionally, you can set up your own custom environment with calls to env_add
-# and env_set:
-#
-# env_add 'PATH', "/path/to/my/tool"
-# env_set 'CMAKE_PREFIX_PATH', "/opt/boost;/opt/xerces"
-# env_set 'CMAKE_INSTALL_PATH', "/opt/orocos"
-#
-# NOTE: Variables set like this are exported in the generated 'env.sh' script.
-#
-
+# Define the github handler
 require 'autoproj/git_server_configuration'
 
-Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
+# Comment this line if you want Autoproj to import the overall shell
+# environment. The default is to reset all build-related environment variables,
+# forcing the build configuration to set them explicitly, to improve
+# repeatability
+Autoproj.isolate_environment
 
+# Display CMake/GCC build messages
+Autobuild::CMake.show_make_messages = true
+
+# Install each package in a separate directory of install/
+#
+# This makes the dependencies stricter, so builds that were fine without this
+# option might fail with it. However, it is also cleaner (allows to remove
+# obsolete package byproducts easily) and allows to turn the
+# delete_obsolete_files_in_prefix CMake option, in which autoproj's cmake
+# handler will remove installed files that are not part of the package anymore.
+Autoproj.config.separate_prefixes = true
+Autobuild::CMake.delete_obsolete_files_in_prefix = Autoproj.config.separate_prefixes?
+
+Autoproj.gitorious_server_configuration('SPACEGIT', 'git.hb.dfki.de', :fallback_to_http => false, :default => 'ssh,ssh', :http_url => 'https://git.hb.dfki.de')
+Autoproj.gitorious_server_configuration('DFKIGIT', 'git.hb.dfki.de', :fallback_to_http => false, :default => 'ssh,ssh', :http_url => 'https://git.hb.dfki.de')
+
+# mars uses QXmlStream to parse xml, which in turn seems (?) to have problems
+# with the decimal separator. so mars has to be consistently executed inside a
+# "LC_NUMERIC=C" environment because all our xml-files use a "." as decimal
+# separator.
+#
+# so we do the least invasive thing and modify the generall rock-env.sh to
+# change the LC_NUMERIC to something which mars needs. this allows starting
+# mars as usual from anywhere inside a rock-shell
+#
+Autobuild.env_set 'LC_NUMERIC','C'
+
+Autobuild.programs['pip'] = '/usr/bin/pip3'
