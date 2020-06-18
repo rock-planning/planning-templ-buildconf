626e2561ef1a5f622506b825e30761b17eb3bc3f
Thomas Roehr <thomas.roehr@dfki.de>
2020-06-18 22:35:08 +0200

    One cannot / should not use isolate environment in combination with new Debian packages releases - with explicitely do the setup through the env.sh

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'autoproj/git_server_configuration'
 # environment. The default is to reset all build-related environment variables,
 # forcing the build configuration to set them explicitly, to improve
 # repeatability
-Autoproj.isolate_environment
+# Autoproj.isolate_environment
 
 # Display CMake/GCC build messages
 Autobuild::CMake.show_make_messages = true
